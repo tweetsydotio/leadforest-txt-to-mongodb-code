618179f46e016896ee142d295d00ae7561cb8386
A.K.M Fozlol Hoq <akmfozlolhoq@gmail.com>
2023-05-08 12:34:37 +0600

    update ins.js

---
+++ ins.js
@@ -0,0 +1,57 @@
+const inputFilePath = './data/hits3/smallfile-2.txt'
+
+const readline = require('readline');
+const fs = require('fs');
+const MongoClient = require('mongodb').MongoClient;
+
+const url = 'mongodb+srv://doadmin:fKH124W03569JLUP@leadforest-twitter-user-data-762016af.mongo.ondigitalocean.com/?authMechanism=DEFAULT';
+
+const batchSize = 10000;
+
+const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true, keepAlive: 1, connectTimeoutMS: 30000 });
+const rl = readline.createInterface({
+  input: fs.createReadStream(inputFilePath) 
+});
+
+let batch = [];
+
+rl.on('line', (line) => {
+  const [emailData, screenNameData] = line.split('ScreenName:');
+  const trimmedEmail = emailData?.split(" - ")[0]?.split(":")[1]?.trim()
+  const trimmedScreenName = screenNameData?.split(' - ')[0]?.trim()
+
+  if(trimmedEmail && trimmedScreenName){
+     batch.push({ screenName:trimmedScreenName, email:trimmedEmail });
+     console.log("pushing",batch.length, { screenName:trimmedScreenName, email:trimmedEmail, fileName: inputFilePath })
+  }
+
+//   if (batch.length === batchSize) {
+//     insertBatch(batch);
+//     batch = [];
+//   }
+});
+
+rl.on('close', () => {
+  if (batch.length > 0) {
+    console.log('total length', batch.length)
+    insertBatch(batch);
+  }
+});
+
+async function insertBatch(batch) {
+  try {
+    await client.connect();
+    const db = client.db('twitter_data');
+    const collection = db.collection('test_users_twitter');
+
+    // create index on screenName field
+    await collection.createIndex({ screenName: 1 });
+    await collection.createIndex({ email: 1 });
+
+    // insert batch
+    rese = await collection.insertMany(batch, { unique : true } );
+    console.log('inserted batch', batch.length,'=', rese.insertedCount)
+  } finally {
+    await client.close();
+  }
+}
